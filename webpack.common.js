9e9e09edef6c3ff33d8b5955b2f6c19856e2ec9e
m0h1m3d <mohisham33@gmail.com>
2024-03-24 08:51:49 +0000

    add rules

--- webpack.common.js
+++ webpack.common.js
@@ -13,4 +13,12 @@ module.exports = {
             template: './src/index.html',
         }),
     ],
-}
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
