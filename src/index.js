d523d7c4a149c0b7186028d5c795b5f45cc4d2ff
m0h1m3d <mohisham33@gmail.com>
2024-03-24 08:44:17 +0000

    add controller module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,78 @@
-const btn = document.createElement("button");
-btn.textContent = 'click me'
-btn.onclick = print
+import './style.css';
+import initialLoad from './initial-load';
+import loadHome from './home';
+import loadMenu from './menu';
+import loadSupport from './support';
 
-function print(){
-    console.log('i"m in index.js');
-};
+initialLoad();
 
-document.body.appendChild(btn);
+const homeBtn = document.querySelector('.home-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const supportBtn = document.querySelector('.support-btn');
+const nav = document.querySelector('nav');
+let currentPage = 'home';
+
+
+homeBtn.addEventListener('click', ()=>{
+    if(currentPage === 'home') return;
+    removePage(currentPage);
+    currentPage = 'home';
+    
+    loadHome();
+
+    const home = document.querySelector('.home');
+    home.classList.add('hidden');
+   setTimeout(() => {
+       home.classList.remove('hidden');
+    }, 300);
+    
+    nav.style.borderRadius = '100px 0px 0px 0px';
+});
+
+menuBtn.addEventListener('click', ()=>{
+    if(currentPage === 'menu') return;
+    removePage(currentPage);
+    currentPage='menu';
+    
+    loadMenu();
+
+    const menu = document.querySelector('.menu');
+    menu.classList.add('hidden');
+   setTimeout(() => {
+       menu.classList.remove('hidden');
+   }, 300);
+
+    nav.style.borderRadius = '100px 0px 0px 100px';
+});
+
+supportBtn.addEventListener('click', ()=>{
+    if(currentPage === 'support') return;
+    removePage(currentPage);
+    currentPage = 'support';
+    
+    loadSupport();
+
+    const support = document.querySelector('.support');
+    support.classList.add('hidden');
+    setTimeout(() => {
+        support.classList.remove('hidden');
+    }, 300);
+
+    nav.style.borderRadius = '20px 0px 0px 20px';
+});
+
+function removePage(name){
+    if(!name) return;
+    const targetSection = document.querySelector(`.${name}`);
+    if(name === 'home'){
+        const home = document.querySelector('.home')
+        const about = document.querySelector('.about');
+        const choose = document.querySelector('.choose');
+        home.remove();
+        about.remove();
+        choose.remove();
+    }else{
+        targetSection.remove();
+    }
+    
+}
