2a01acdae9f75a91b81c73a7cf58fd00d1898d6a
m0h1m3d <mohisham33@gmail.com>
2024-03-24 10:11:14 +0000

    switch img markup to div elements

--- src/menu.js
+++ src/menu.js
@@ -6,7 +6,7 @@ function loadMenu(){
             <h1 class="menu-heading">Food <span class="orange">Menu</span></h1>
             <div class="food">
                 <div class="order">
-                    <img src="./Assets/imgs/grilled-spicy-chicken-wings-with-ketchup-and-french-fries-ai-generative-free-png.webp" alt="">
+                    <div class="order-one"></div>
                     <div class="order-info">
                         <h2>Breakfast Meal</h2>
                         <button class="order-info-btn">Add to cart</button>
@@ -16,7 +16,7 @@ function loadMenu(){
                     </div>
                 </div>
                 <div class="order">
-                    <img src="./Assets/imgs/about.webp" alt="">
+                <div class="order-two"></div>
                     <div class="order-info">
                         <h2>Lunch Meal</h2>
                         <button class="order-info-btn">Add to cart</button>
@@ -26,7 +26,7 @@ function loadMenu(){
                     </div>
                 </div>
                 <div class="order">
-                    <img src="./Assets/imgs/about.webp" alt="">
+                <div class="order-three"></div>
                     <div class="order-info">
                         <h2>Dinner Meal</h2>
                         <button class="order-info-btn">Add to cart</button>
